a73e0717d3e1b9a22872c573ca42240a4f555025
Kingsley Victor <javaprodigy56@gmail.com>
2019-04-02 12:02:56 +0100

    New commit

--- src/main/java/com/spring/rest/amqp/broking/listeners/RabbitListenerConfiguration.java
+++ src/main/java/com/spring/rest/amqp/broking/listeners/RabbitListenerConfiguration.java
@@ -1,6 +1,8 @@
 package com.spring.rest.amqp.broking.listeners;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+//import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
 import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +15,11 @@ public class RabbitListenerConfiguration implements RabbitListenerConfigurer {
     @Bean
     public TopicExchange exhange(@Value("${service.exchangeKey}") String exchanger) {
         return new TopicExchange(exchanger);
+    }
+
+    @Bean
+    public Queue queue(@Value("${service.queue}") String queue) {
+        return new Queue(queue, true);
     }
 
     @Override
