b6f1b055670c75e362fbd49ede8ff9f4993b3de5
Kingsley Victor <javaprodigy56@gmail.com>
2019-04-02 12:17:35 +0100

    New commit

--- src/main/java/com/spring/rest/amqp/broking/listeners/RabbitListenerConfiguration.java
+++ src/main/java/com/spring/rest/amqp/broking/listeners/RabbitListenerConfiguration.java
@@ -1,5 +1,7 @@
 package com.spring.rest.amqp.broking.listeners;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 //import org.springframework.amqp.rabbit.annotation.Queue;
@@ -8,6 +10,8 @@ import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
 
 @Configuration
 public class RabbitListenerConfiguration implements RabbitListenerConfigurer {
@@ -22,9 +26,28 @@ public class RabbitListenerConfiguration implements RabbitListenerConfigurer {
         return new Queue(queue, true);
     }
 
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange,
+            @Value("${service.profileCreatedKey}") String profileCreatedKey) {
+        return BindingBuilder.bind(queue).to(exchange).with(profileCreatedKey);
+    }
+
+    @Bean
+    public MappingJackson2MessageConverter converter() {
+        return new MappingJackson2MessageConverter();
+    }
+
+    @Bean
+    public DefaultMessageHandlerMethodFactory factory() {
+        DefaultMessageHandlerMethodFactory factory = new DefaultMessageHandlerMethodFactory();
+        factory.setMessageConverter(converter());
+
+        return factory;
+    }
+
     @Override
     public void configureRabbitListeners(RabbitListenerEndpointRegistrar registrar) {
-
+        registrar.setMessageHandlerMethodFactory(factory());
     }
 
 }
